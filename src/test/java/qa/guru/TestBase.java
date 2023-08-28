90fd42f86f65f3f31477fa483dd29f6edd40e2b7
Chingiz <chingiz-askarov@list.ru>
2023-08-28 23:34:39 +0300

    refactor method name

--- src/test/java/qa/guru/TestBase.java
+++ src/test/java/qa/guru/TestBase.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
     @BeforeAll
-    static void beforeall() {
+    static void beforeAll() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\ching\\OneDrive\\Рабочий стол\\Driver\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         System.setProperty("selenide.browser", "Chrome");
 
