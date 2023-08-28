1006d8ee39d7877ab58b9620c0415bd1783f1b6b
Chingiz <75143704+Chingiz-Rhino@users.noreply.github.com>
2023-08-28 23:50:47 +0300

    Update AnnotatedSteps.java

--- src/test/java/qa/guru/AnnotatedSteps.java
+++ src/test/java/qa/guru/AnnotatedSteps.java
@@ -3,7 +3,7 @@ package qa.guru;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class AnnotatedSteps extends TestBase{
+public class AnnotatedSteps {
 
     private static final String REPOSITORY = "Chingiz-Rhino/Allure_Reports_21";
     private static final String ISSUE = "First Issues";
